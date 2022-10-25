443147d4b25d5b60f9a40f58bcbaf00c69db71b5
cooperrunyan <cooperrunyanspam@gmail.com>
2022-10-24 22:52:23 -0700

    Initial

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  webpack: (config, { dev, isServer }) => {
+    // Note, preact is only enabled for production builds (`next build`)
+    if (!dev && !isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      };
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+const Page: React.FC = () => {
+  return <div>hello world</div>;
+};
+
+export default Page;
