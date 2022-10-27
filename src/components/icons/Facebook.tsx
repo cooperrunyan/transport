fcee02de08ad08b9501f57d4163cfb3f55019bf6
cooperrunyan <cooperrunyanspam@gmail.com>
2022-10-27 16:10:05 -0700

    Update icons

--- src/components/icons/Facebook.tsx
+++ src/components/icons/Facebook.tsx
@@ -1,12 +1,12 @@
 import { SVGAttributes } from 'react';
 
 export const Facebook: React.FC<SVGAttributes<SVGElement>> = props => (
-  <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+  <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" color="var(--grey-10)" {...props}>
     <path
       fillRule="evenodd"
       clipRule="evenodd"
       d="M45 24.1266C45 12.5297 35.5969 3.12659 24 3.12659C12.4031 3.12659 3 12.5297 3 24.1266C3 34.6078 10.6781 43.2957 20.7188 44.8725V30.1988H15.3853V24.1266H20.7188V19.5C20.7188 14.2378 23.8547 11.3288 28.6509 11.3288C30.9487 11.3288 33.3525 11.7394 33.3525 11.7394V16.9078H30.7031C28.0959 16.9078 27.2803 18.526 27.2803 20.1891V24.1266H33.1041L32.1741 30.1988H27.2812V44.8744C37.3219 43.2985 45 34.6107 45 24.1266Z"
-      fill="var(--grey-10)"
+      fill="currentColor"
     />
   </svg>
 );
