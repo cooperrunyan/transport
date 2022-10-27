0cd85b2fa90dccd9bebccd548ef7eab5a14c15fc
cooperrunyan <cooperrunyanspam@gmail.com>
2022-10-26 19:10:48 -0700

    implement social sign on providers

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,8 +5,18 @@ import { Google } from '../../components/icons/Google';
 import { Logo } from '../../components/icons/Logo';
 import style from '../../style/pages/login.module.scss';
 
-import { FacebookAuthProvider, getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
-import { app } from '../../services/firebase/client';
+import { FacebookAuthProvider, getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { collection, doc, setDoc } from 'firebase/firestore';
+import { app, db } from '../../services/firebase/client';
+
+const signinWith = (p: any) =>
+  signInWithPopup(getAuth(app), new p()).then(cred => {
+    return setDoc(doc(collection(db, 'users'), cred.user.uid), {
+      username: cred.user.email!.split('@').slice(0, -1).join('@'),
+      email: cred.user.email,
+      tz: 'MST',
+    });
+  });
 
 const Page: React.FC = () => {
   return (
@@ -21,19 +31,19 @@ const Page: React.FC = () => {
               <Email /> Sign in with email
             </a>
           </li>
-          <li onClick={() => signInWithRedirect(getAuth(app), new GithubAuthProvider())}>
+          <li onClick={() => signinWith(GithubAuthProvider)}>
             <a>
               <Github />
               Sign in with Github
             </a>
           </li>
-          <li onClick={() => signInWithRedirect(getAuth(app), new GoogleAuthProvider())}>
+          <li onClick={() => signinWith(GoogleAuthProvider)}>
             <a>
               <Google />
               Sign in with Google
             </a>
           </li>
-          <li onClick={() => signInWithRedirect(getAuth(app), new FacebookAuthProvider())}>
+          <li onClick={() => signinWith(FacebookAuthProvider)}>
             <a>
               <Facebook /> Sign in with Facebook
             </a>
